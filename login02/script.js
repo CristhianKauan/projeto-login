316dcd16232367914efed9f11d45f3a5b65edd2f
CristhianKauan <cristhiandev20@gmail.com>
2023-01-18 10:44:16 -0300

    update

---
+++ login02/script.js
@@ -0,0 +1,19 @@
+
+var n=0
+function mudar(){
+
+if(n%2==0)
+{login.style.background = '#25225C' 
+login.style.color='white'
+entrar.style.background='black'
+entra.style.background='black'
+n=1}
+else
+{login.style.background = 'white'
+login.style.color='#5650DB'
+entrar.style.background='#25225C'
+entra.style.background='#25225C'
+n=2}
+
+
+}
