2897c147ced80cce8d6b40dfa5bd6114e1d4c95f
CristhianKauan <cristhiandev20@gmail.com>
2023-01-19 16:37:07 -0300

    update

---
+++ script.js
@@ -0,0 +1,52 @@
+
+
+function mostrar(x){
+    
+
+    switch(x){
+
+    case 1:
+
+    var framer=document.getElementById('framep')
+    framer.classList.add('mostrar');
+    break
+
+    case 2:
+
+    var framer=document.getElementById('framer')
+    framer.classList.add('mostrar');
+    break
+
+    case 3:
+
+    var framer=document.getElementById('framey')
+    framer.classList.add('mostrar');
+    break
+    }
+
+}
+
+function esconder(x){
+
+    switch(x){
+        case 1:
+            var framer=document.getElementById('framep')
+            framer.classList.remove('mostrar');
+        break
+
+        case 2:
+            var framer=document.getElementById('framer')
+            framer.classList.remove('mostrar');
+        break
+        
+        case 3:
+            var framer=document.getElementById('framey')
+            framer.classList.remove('mostrar');
+        break
+
+
+
+    }
+
+}
+
